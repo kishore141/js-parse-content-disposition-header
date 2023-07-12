c77f4cc36ff84533e054eac506b5bedd57381834
kishore <kjkishore141@gmail.com>
2023-07-12 13:56:36 +0530

    Added Constants

---
+++ src/constants.ts
@@ -0,0 +1,17 @@
+const TYPE_INLINE = 'inline';
+const TYPE_ATTACHMENT = 'attachment';
+const TYPE_UNKNOWN = 'unknown';
+const FILENAME = "filename";
+const FILENAME_SEPARATOR = '"'
+const FILENAME_ENCODED = "filename*";
+const FILENAME_ENCODED_PREFIX = "filename*=utf-8''";
+
+export = {
+    TYPE_INLINE,
+    TYPE_ATTACHMENT,
+    TYPE_UNKNOWN,
+    FILENAME,
+    FILENAME_SEPARATOR,
+    FILENAME_ENCODED,
+    FILENAME_ENCODED_PREFIX
+}
